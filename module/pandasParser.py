6def57102f5851260f8daadb9babcaba7b1cd980
KonanHYM <724417787@qq.com>
2017-09-17 09:24:42 +0800

    version2

--- module/pandasParser.py
+++ module/pandasParser.py
@@ -2,51 +2,36 @@
 import numpy as np
 import pandas as pd
 import math
-
 from config import BEINGMATE
-Main_Business = {
-    '营业总收入-百万元':'营业总收入',
-    '产品':'',
-    '奶粉类':'',
-    '其他主营业务':'',
-    '其他业务':'',
 
-    '毛利率':'毛利率',
-    '产品':'',
-    '奶粉类':'',
-    '其他主营业务':'',
-    '其他业务':'',
+ProfitList = [
+    u'营业总收入',
+    u'营业收入',
+    u'营业成本',
+    u'营业税金及附加',
+    u'销售费用',
+    u'管理费用',
+    u'财务费用',
+    u'资产减值损失',
+    u'投资净收益',
+    u'营业利润',
+    u'营业外收支',
+    u'利润总额',
+    u'减：所得税',
+    u'减：少数股东损益',
+    u'归属于母公司所有者的净利润',
+]
 
-    '毛利-百万元':'毛利',
-    '产品':'',
-    '奶粉类':'',
-    '其他主营业务':'',
-    '其他业务':'',
+CashFlowList = [
+    u'销售商品收现',
+    u'经营现金流',
+]
 
-    '营业成本-百万元':'营业成本',
-    '产品':'',
-    '奶粉类':'',
-    '其他主营业务':'',
-    '其他业务':'',
-}
-
-Profit = {
-    '营业总收入':,
-    '营业收入':'',
-    '营业成本':'',
-    '营业税金及附加':'',
-    '销售费用':'',
-    '管理费用':'',
-    '财务费用':'',
-    '资产减值损失':'',
-    '投资净收益':'',
-    '营业利润':'',
-    '营业外收支':'',
-    '利润总额':'',
-    '减：所得税':'',
-    '减：少数股东损益':'',
-    '归属于母公司所有者的净利润':'',
-}
+BalanceSheetList = [
+    u'应收票据',
+    u'应收账款',
+    u'预收款项',
+]
 
 def getType(final_types):
     if 'Q' in str(final_types):
@@ -58,23 +43,88 @@ def getType(final_types):
     else:
         return u'年报'
 
+def getSeason(final_types):
+    if 'Q' in str(final_types):
+        season = final_types.split('Q')[1]
+        if season == '1':
+            return u'第一季度'
+        if season == '2':
+            return u'第二季度'
+        if season == '3':
+            return u'第三季度'
+        if season == '4':
+            return u'第四季度'
+        else:
+            return
+    else:
+        return
+
 #处理最终数据报表
 def finalParser():
     final = pd.read_excel(BEINGMATE['Final'])
-    financialSummarySeason = pd.read_excel(BEINGMATE['Financial_Summary_Season'])#读取财务摘要单季表
     mainBusiness = pd.read_excel(BEINGMATE['Main_Business'])#读取主营业务表
-
+    profit = pd.read_excel(BEINGMATE['Profit'])#读取利润表
+    profitSeason = pd.read_excel(BEINGMATE['Profit_Season'])#读取利润表(单季)
+    balanceSheet = pd.read_excel(BEINGMATE['Balance_Sheet'])#读取资产负债表
     for final_types in final.columns:
         year = '20' + str(final_types)[0:2]
         reptype = getType(final_types = final_types)
-
+        season = getSeason(final_types = final_types)
         #处理主营业务
         data = detailParser(mainBusiness, year, reptype)
-        dataParser(final, data, dictFile)
-        # for item in mainBusiness.columns:
-        #     print item.split('-')[0]
-        #     print mainBusiness.ix[u'营业总收入']
-        #     # print financialSummarySeason[u'2017-06-30'][u'报告类型']
+        if not data is None:
+            #营业收入&营业总收入
+            final[final_types][1] = data[u'营业总收入' : u'营业成本'][0]
+            final[final_types][3] = data[u'营业总收入' : u'营业成本'][0]
+            final[final_types][4] = data[u'营业总收入' : u'营业成本'][1]
+            final[final_types][5] = data[u'营业总收入' : u'营业成本'][2]
+            final[final_types][6] = data[u'营业总收入' : u'营业成本'][3]
+            final[final_types][7] = data[u'营业总收入' : u'营业成本'][4]
+            #营业成本
+            final[final_types][64] = data[u'营业成本' : u'毛利'][0]
+            final[final_types][65] = data[u'营业成本' : u'毛利'][2]
+            final[final_types][66] = data[u'营业成本' : u'毛利'][3]
+            final[final_types][67] = data[u'营业成本' : u'毛利'][4]
+            #毛利
+            final[final_types][52] = data[u'毛利' : u'毛利率(%)'][0]
+            final[final_types][53] = data[u'毛利' : u'毛利率(%)'][2]
+            final[final_types][54] = data[u'毛利' : u'毛利率(%)'][3]
+            final[final_types][55] = data[u'毛利' : u'毛利率(%)'][4]
+            #毛利率
+            final[final_types][48] = data[u'毛利率(%)' : u'显示币种'][0]
+            final[final_types][49] = data[u'毛利率(%)' : u'显示币种'][2]
+            final[final_types][50] = data[u'毛利率(%)' : u'显示币种'][3]
+            final[final_types][51] = data[u'毛利率(%)' : u'显示币种'][4]
+        #处理利润表
+        data = detailParser(profit, year, reptype)
+        if not data is None:
+            for data_index in data.index:
+                if not isinstance(data_index, float):
+                    data_strip = data_index.strip()
+                    if data_strip in ProfitList:
+                        final[final_types][data_strip] = data[data_index]
+        #处理利润表（单季）
+        data = seasonParser(profitSeason, year, season)
+        if not data is None:
+            for data_index in data.index:
+                if not isinstance(data_index, float):
+                    data_strip = data_index.strip()
+                    if data_strip in ProfitList:
+                        final[final_types][data_strip] = data[data_index]
+
+        #处理资产负债表
+        data = detailParser(balanceSheet, year, reptype)
+        if not data is None:
+            for data_index in data.index:
+                if not isinstance(data_index, float):
+                    data_strip = data_index.strip()
+                    if data_strip in BalanceSheetList:
+                        final[final_types][data_strip] = data[data_index]
+    # print final
+    #生成最终版EXCEL文件
+    writer = pd.ExcelWriter('../files/output/1.xlsx')
+    final.to_excel(writer,'Sheet1')
+    writer.save()
 
 #输入一个DataFrame 返回对应到Final具体哪一列
 def detailParser(dataF, year, reptype):
@@ -83,8 +133,12 @@ def detailParser(dataF, year, reptype):
             if reptype == dataF[item][u'报告期']:
                 return dataF[item]
 
-def dataParser(final, data, dictFile):
-    pass
+#输入一个DataFrame 到单季表 返回对应到Final具体哪一列
+def seasonParser(dataF, year, season):
+    for item in dataF.columns:
+        if year in str(item):
+            if season == dataF[item][u'报告期']:
+                return dataF[item]
 
 if __name__ == '__main__':
     finalParser()
